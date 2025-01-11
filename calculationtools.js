729b03cc2ec1ad8ce9562543d18ca4dead0db248
IIIJonasIII <jonasolsson86@gmail.com>
2025-01-11 12:02:35 +0100

    javascript, lite uppdateringar

--- calculationtools.js
+++ calculationtools.js
@@ -1,4 +1,4 @@
-
+//Traluträknare
 const trallskruvm2 = 30;
 let antalm2;
 let resultat;
@@ -9,3 +9,18 @@ document.getElementById("calculate").onclick = function(){
     resultat = trallskruvm2 * antalm2;
     document.getElementById("trallresultat").textContent = resultat + " trallskruv";
 }
+
+
+//Vilken dragare?
+const currentImage = document.getElementById('current-image');
+const knapp1 = document.getElementById('knapp1');
+const knapp2 = document.getElementById('knapp2');
+const knapp3 = document.getElementById('knapp3');
+
+function changeImage(imageSrc) {
+    currentImage.src = imageSrc;
+}
+
+knapp1.addEventListener('click', () => changeImage('product-images/Black & Decker BCD001C1 - 890kr.jpg'));
+knapp2.addEventListener('click', () => changeImage('product-images/STANLEY FatMax -  2690kr.jpg'));
+knapp3.addEventListener('click', () => changeImage('product-images/Makita DDF484RTJ - 4790kr.jpg'));
